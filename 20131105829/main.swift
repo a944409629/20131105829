b50fb453a2a0092e32524b9fee0b96c6bc6d7115
a944409629 <944409629@qq.com>
2016-03-10 11:44:45 +0800

    SWIFT冒泡排序
    数字按照大小排序输出

--- 20131105829/main.swift
+++ 20131105829/main.swift
@@ -8,10 +8,30 @@
 
 import Foundation
 
-var i = 1
-while i<=10
-{
-    print("\(i)")
-    i = i+1
-}
+var array = [10,20,12,35,7,67,24,133,5,98,]
 
+print("排序前的值：")
+
+for item in array
+{
+    var ii = item
+    print(ii)
+     }
+
+ for var i = 0; i < array.count - 1; ++i {
+     for var j = 0; j < array.count - 1 - i; ++j{
+         if array[j] > array[j + 1] {
+             var temp = array[j + 1]
+             array[j + 1] = array[j]
+             array[j] = temp
+             }
+         }
+     }
+
+ print("排序后的值：")
+
+ for item in array
+ {
+     var ii = item
+     print(ii)
+     }
